09a106dbc581c572079722908c6a3d205cbd2315
Afalls89 <andrewfalls@outlook.com>
2020-03-22 12:00:28 +0000

    added CV as jpg files

--- portfolio/src/components/CV.jsx
+++ portfolio/src/components/CV.jsx
@@ -2,13 +2,16 @@ import React from "react";
 
 // import { Document } from "react-pdf/dist/entry.parcel";
 // import { Document, pdfjs } from "react-pdf";
-import { Document, Page } from "react-pdf/dist/entry.webpack";
+
+// import { Document, pdfjs } from "react-pdf/dist/entry.webpack";
 // pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const CV = () => {
 	return (
 		<div className="content">
-			<Document file="AndrewFallsCV2020.pdf" />
+			<img className="cvPic" src="CVpage1.jpg" />
+			<img className="cvPic" src="CVpage2.jpg" />
+			{/* <Document file="AndrewFallsCV2020.pdf" /> */}
 		</div>
 	);
 };
